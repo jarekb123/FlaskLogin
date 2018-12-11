bc688d5ee6dc325df413e4346ac4f06c8d1c941c
j.butajlo <j.butajlo@samsung.com>
2018-12-11 15:03:49 +0100

    update app.py environment settings

--- webapp/app.py
+++ webapp/app.py
@@ -5,7 +5,7 @@ from flask_bcrypt import Bcrypt
 
 
 app = Flask(__name__)
-app.config.from_object(os.getenv('APP_SETTINGS', 'webapp.config.DevelopmentConfig'))
+app.config.from_object(os.getenv('APP_SETTINGS', 'webapp.config.HerokuConfig'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
