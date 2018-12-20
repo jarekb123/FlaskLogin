5a4c55f466bdf8da6973f28605fe1041faff8b1a
j.butajlo <j.butajlo@samsung.com>
2018-12-20 13:57:08 +0100

    update after_request 2

--- webapp/app.py
+++ webapp/app.py
@@ -23,6 +23,7 @@ def after_request(response):
     response.headers.add('Access-Control-Allow-Origin', '*')
     response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
     response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    response.headers.add('Access-Control-Allow-Credentials', 'True')
     return response
 
 
