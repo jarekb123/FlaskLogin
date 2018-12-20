f95945d6be25eb5cc4caaf914c022015344eb52b
j.butajlo <j.butajlo@samsung.com>
2018-12-20 13:33:11 +0100

    remove after_requests decorator

--- webapp/app.py
+++ webapp/app.py
@@ -9,11 +9,6 @@ app.config.from_object('webapp.config.HerokuConfig')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
-@app.after_request
-def after_request(response):
-    response.headers.add('Access-Control-Allow-Origin', '*')
-
-
 api = Api(app)
 
 db = SQLAlchemy(app)
