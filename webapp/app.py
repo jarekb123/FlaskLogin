548a5b442804ec93a755f3933256aa3f710196dd
j.butajlo <j.butajlo@samsung.com>
2018-12-20 14:28:37 +0100

    fix app.py

--- webapp/app.py
+++ webapp/app.py
@@ -1,4 +1,7 @@
 import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
