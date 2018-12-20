195661a29f43523624c98ef83121b14db4e24968
j.butajlo <j.butajlo@samsung.com>
2018-12-20 13:41:53 +0100

    add flask-cors

--- webapp/app.py
+++ webapp/app.py
@@ -2,12 +2,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_restplus import Api
+from flask_cors import CORS
 
 
 app = Flask(__name__)
 app.config.from_object('webapp.config.HerokuConfig')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+cors = CORS(app, resources={r"/user/*": {"origins": ""}})
 
 api = Api(app)
 
